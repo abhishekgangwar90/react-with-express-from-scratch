3c77685139ed16abd1f74d63065ea72b6ac99d31
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-05-26 23:43:20 +0530

    feat: added basic scss support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
 function Welcome() {
-  return <h1>Hello World from React App from scratch</h1>;
+  return <h1 className="app">Hello World from React App from scratch</h1>;
 }
 ReactDOM.render(<Welcome />, document.getElementById('root'));
