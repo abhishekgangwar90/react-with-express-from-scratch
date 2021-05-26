3c130caa39517bb7b14e6aa670ebc6b5f54d8114
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-05-26 23:40:25 +0530

    feat: finished simple webpack config part 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Welcome() {
-  return <h1>Hello World from React boilerplate</h1>;
+  return <h1>Hello World from React App from scratch</h1>;
 }
 ReactDOM.render(<Welcome />, document.getElementById('root'));
---
+++ webpack/webpack.base.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
