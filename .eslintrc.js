4d61b2432be634d8bee15cad80d55a3b4df0add8
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-05-26 23:21:49 +0530

    feat: setting up babel webpack part 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,26 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   plugins: ['prettier'], // TODO - use this plugin,
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-props-no-spreading': [
+      0,
+      {
+        html: 'ignore',
+        custom: 'ignore',
+        explicitSpread: 'ignore',
+      },
+    ],
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
   globals: {
     window: true,
     document: true,
